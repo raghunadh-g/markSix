271d164f97f162ff4bbad9090bb489c1fa1e75a9
raghunadh-g <67798609+raghunadh-g@users.noreply.github.com>
2021-09-16 23:51:53 +0530

    styles:add styles and banana flabour to the site

--- app.js
+++ app.js
@@ -1,2 +1,18 @@
-var name = prompt("enter your name");
-alert("your name is:" + name);
+var primaryButton = document.querySelector("#btn-primary");
+var txtArea = document.querySelector('[id="paragraph"]');
+var outputDiv = document.querySelector("#outputDiv");
+// var serverURL = "https://api.funtranslations.com/translate/klingon.json";
+var serverURL = "https://api.funtranslations.com/translate/romulan.json";
+
+function constructURL(text) {
+  return serverURL + "?text=" + text;
+}
+
+primaryButton.addEventListener("click", function handler() {
+  let inputText = txtArea.value;
+  fetch(constructURL(inputText))
+    .then((res) => res.json())
+    .then((json) => (outputDiv.innerText = json.contents.translation))
+    // .then((json) => console.log(json))
+    .catch((error) => console.log("hellooooo"));
+});
